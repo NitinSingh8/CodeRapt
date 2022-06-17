70e69fd73ae77716ea554eb66a75814bf3dad4ca
Nitin Singh <nitinsinghsaini8@gmail.com>
2022-06-17 11:42:26 +0530

    Create L15.py

---
+++ Python Complete Course/L15.py
@@ -0,0 +1,7 @@
+# Lecture 15
+# if you are using this file then you have to first create a Calculator.py file and inside that file you have to write add() function for adding two number
+import Calculator
+
+s = Calculator.add(10,5)
+
+print(s)
