b732329feefbd5d8453e067c4a9cfd1e90cdfbc3
Nitin Singh <nitinsinghsaini8@gmail.com>
2022-06-25 10:59:01 +0530

    Created Project2.py

---
+++ Python Complete Course/Project/Project2.py
@@ -0,0 +1,22 @@
+# Currency Converter
+
+from currency_converter import CurrencyConverter
+
+def main():
+    print("This is a currency converter project.")
+    print("You can use these currency ('INR','USD','EUR', 'JPY') ")
+
+    amount = float(input("Enter the amount you want to convert : "))
+
+    curr_currency = input("Enter the currency which you currently have : ")
+    want_currency = input("Enter the currency you want to have : ")
+
+    # CurrencyConverter.convert()
+    c = CurrencyConverter()
+
+    converted_amount = c.convert(amount,curr_currency,want_currency)
+
+    print(f"The {amount} {curr_currency} in {want_currency} is : {converted_amount}")
+
+
+main()
