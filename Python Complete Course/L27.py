5138c7e64058668ce08b19e479a53f596aa83e86
Nitin Singh <nitinsinghsaini8@gmail.com>
2022-07-14 11:06:46 +0530

    Create L27.py

---
+++ Python Complete Course/L27.py
@@ -0,0 +1,19 @@
+# Lecture 27
+
+# num = int(input("Enter the number(0-50) : "))
+#
+# if num<0 or num>50:
+#     # print("num is out of range")
+#     raise ValueError("num is out of range")
+#
+# else:
+#     print("num is a valid")
+
+
+# print("hello")
+
+
+
+num = int(input("Enter the number : "))
+
+assert num==10, ("num is not equal to 10")
